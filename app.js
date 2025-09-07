d7b3edcc03e98d3bb7bf62df56ed393fb708ce08
Manoj <gdmh7991@gmail.com>
2025-09-07 21:38:17 +0530

    vercel updated

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const connectDB = require('./config/db');
-const routes = require('./routes');
+const connectDB = require('./src/config/db');
+const routes = require('./src/routes');
 
 const app = express();
 
